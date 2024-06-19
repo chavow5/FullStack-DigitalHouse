0777a1064410d0263e9d619afe107d413c4d39dd
chavow5 <davidramirezw5@gmail.com>
2024-06-19 02:18:59 -0300

    microdesafio clase 17 terminado

--- clases/primer-proyecto/app.js
+++ clases/primer-proyecto/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 app.listen(3000,() => {
-    console.log('servidor corriendo en el puerto 3000');
+    console.log('servidor corriendo en el puerto 3000')
+    console.log('localhost:3000');;
+
 })
 
 // para poner la app en publico y estatico
 const path = require('path')
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
+
+// para llamar al home.html uso app.get
+// app.get('/', (req, res) =>{
+//     res.sendFile(path.resolve(__dirname, 'clases\primer-proyecto\views\home.html'));
+// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    //me entro con join no con resolve
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Carpeta de trabajo de Digital House 
+
+link playground : https://playground.digitalhouse.com
+comienzo desde la clase 17, utilizacion de github como practica
+
+## carpetas admitidas
+
+{node modules}
