9a361a6f1973269043c914b2d0a672c06b2df31f
chavow5 <davidramirezw6@gmail.com>
2024-06-26 21:59:28 -0300

    agrego el login e imagenes

--- clases/primer-proyecto/app.js
+++ clases/primer-proyecto/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+
+// llamar al servidor desde un puerto
 app.listen(3000,() => {
     console.log('servidor corriendo en el puerto 3000')
     console.log('localhost:3000');;
@@ -11,11 +13,29 @@ const path = require('path')
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-// para llamar al home.html uso app.get
+// para llamar al index.html uso app.get
 // app.get('/', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname, 'clases\primer-proyecto\views\home.html'));
-// })
+//     res.sendFile(path.resolve(__dirname, 'clases\primer-proyecto\views\index.html'));
+// });
+
+// Rutas de Archivos
+// principal index
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    res.sendFile(path.join(__dirname, './views/index.html'));
     //me entro con join no con resolve
 });
+
+// login
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+// registro 
+app.get('./register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+// formulario
+app.post('/sumbit-form', (req, res) =>{
+    res.redirect('/');
+}); // toma el proceso del formulario y vulve a la carpeta anterior 
